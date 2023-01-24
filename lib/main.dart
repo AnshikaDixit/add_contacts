955951462b407fc21bb1797419c9eef25e62af34
anshikadixit09 <anshika.dixit@dianapps.com>
2023-01-24 09:47:48 +0530

    managing state with a singleton value notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -22,33 +23,45 @@ class MyApp extends StatelessWidget {
 }
 
 class Contact {
+  final String id; //to identify contacts using uuid package
   final String name;
-  const Contact({required this.name});
+  Contact({required this.name}) : id = const Uuid().v4();
 }
 
-class ContactBook {
+//homepage doesnt know how to display the contacts so for that we make the use of valuenotifier
+class ContactBook extends ValueNotifier<List<Contact>> {
   //singleton - one instance in entire app
-  ContactBook._sharedInstance(); //private const.
+  ContactBook._sharedInstance()
+      : super([]); //private const. and we will manage a empty list here
   static final ContactBook _shared = ContactBook._sharedInstance();
 
   factory ContactBook() => _shared;
 
-  //storage for contacts
-  final List<Contact> _contacts = [];
+  //change contactbook to value notifier beacuse we are having values now
+  int get length => value.length;
 
-  int get length => _contacts.length;
-
+  //instead of using _contact.add we will use value.add and notifylisteners
   void add({required Contact contact}) {
-    _contacts.add(contact);
+    // value.add(contact);
+    // notifyListeners();
+    final contacts = value;
+    contacts.add(contact);
+    notifyListeners();
   }
 
   void remove({required Contact contact}) {
-    _contacts.remove(contact);
+    // value.remove(contact);
+    // notifyListeners();
+    final contacts = value;
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      notifyListeners();
+    }
   }
 
   //return contact using its index
   Contact? contact({required int atIndex}) =>
-      _contacts.length > atIndex ? _contacts[atIndex] : null;
+      value.length > atIndex ? value[atIndex] : null;
 }
 
 class HomePage extends StatelessWidget {
@@ -57,19 +70,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //instance of ContactBook() to display contacts
-    final contactBook = ContactBook();
+    // final contactBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: ListView.builder(
-          itemCount: contactBook.length,
-          itemBuilder: (context, index) {
-            final contact = contactBook.contact(atIndex: index)!;
-            return ListTile(
-              //for every contact we want to display the ListTile and for that we will use contact atindex function defined above to create contact
-              title: Text(contact.name),
-            );
+
+      //change listview.builder to valuelistenablebuilder
+      body: ValueListenableBuilder(
+          valueListenable: ContactBook(),
+          builder: (contact, value, child) {
+            final contacts = value as List<Contact>;
+            return ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  // final contact = contactBook.contact(atIndex: index)!;
+                  final contact = contacts[index];
+                  return Dismissible(
+                    onDismissed: ((direction) {
+                      ContactBook().remove(contact: contact);
+                    }),
+                    key: ValueKey(contact.id),
+                    child: Material(
+                      color: Colors.white,
+                      elevation: 6.0,
+                      child: ListTile(
+                        //for every contact we want to display the ListTile and for that we will use contact atindex function defined above to create contact
+                        title: Text(contact.name),
+                      ),
+                    ),
+                  );
+                });
           }),
 
       //FAB to add new contacts to the list to be displayed
@@ -93,6 +124,19 @@ class NewContactView extends StatefulWidget {
 
 class _NewContactViewState extends State<NewContactView> {
   late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
